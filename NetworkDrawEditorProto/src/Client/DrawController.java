4cfb719bdc4c97695bc9683f99ac203f83ff9911
ytx1313 <95043210+ytx1313@users.noreply.github.com>
2021-12-24 13:46:25 +0900

    Update DrawController.java

--- NetworkDrawEditorProto/src/Client/DrawController.java
+++ NetworkDrawEditorProto/src/Client/DrawController.java
@@ -2,67 +2,6 @@ package Client;
 
 import Public.*;
 
-import java.awt.*;
-import java.awt.event.*;
-
-////////////////////////////////////////////
-// Controller
-public class DrawController implements MouseListener,MouseMotionListener,KeyListener,ActionListener {
-    protected DrawModel model;
-    protected int dragStartX,dragStartY;
-
-    public DrawController(DrawModel m) {
-        model = m;
-    }
-    
-    public void mouseClicked(MouseEvent e) { }
-    public void mousePressed(MouseEvent e) {
-        dragStartX = e.getX(); dragStartY = e.getY();
-        model.createFigure(dragStartX,dragStartY);
-    }
-    public void mouseDragged(MouseEvent e) {
-        model.reshapeFigure(dragStartX,dragStartY,e.getX(),e.getY());
-    }
-    public void mouseReleased(MouseEvent e) {
-        model.sendFigure(model.getDrawingFigure());
-        model.deleteDrawingFigure();
-    }
-    public void mouseEntered(MouseEvent e) { }
-    public void mouseExited(MouseEvent e) { }
-    public void mouseMoved(MouseEvent e) { }
-    public void keyTyped(KeyEvent e) {
-        switch(e.getKeyChar()) {
-            case 'b':
-                model.setColor(Color.BLACK);
-                break;
-            case 'y':
-                model.setColor(Color.YELLOW);
-                break;
-            case 'g':
-                model.setColor(Color.GREEN);
-                break;
-            case 'r':
-                model.setColor(Color.RED);
-                break;
-            case 'f':
-                model.setFigShape(FigShape.FILLRECT);
-                break;
-            case 'e':
-                model.setFigShape(FigShape.RECTANGLE);
-                break;
-        }
-    }
-    public void keyPressed(KeyEvent e) { }
-    public void keyReleased(KeyEvent e) { }
-    public void actionPerformed(ActionEvent e) { }
-}
-/* 
-// model.getMode(), model.setHandle(), model.getHandle(), model.selectFigure(int x, int y) の追加
-// model.setDrawingFigure(Figure f) に setChanged();notifyObservers(); の追加
-package Client;
-
-import Public.*;
-
 import java.awt.event.*;
 
 ////////////////////////////////////////////
@@ -248,4 +187,3 @@ public class DrawController implements MouseListener,MouseMotionListener,KeyList
 
     public void actionPerformed(ActionEvent e) { }
 }
-*/
