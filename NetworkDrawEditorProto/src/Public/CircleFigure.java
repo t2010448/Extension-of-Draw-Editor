787de79d31156d95d40bf3d9b5bd78e127f1adc6
ytx1313 <95043210+ytx1313@users.noreply.github.com>
2021-12-24 14:43:03 +0900

    Add files via upload

---
+++ NetworkDrawEditorProto/src/Public/CircleFigure.java
@@ -0,0 +1,18 @@
+package Public;
+
+import java.awt.*;
+
+public class CircleFigure extends Figure{
+    
+    public CircleFigure(int x, int y, int w, int h, Color c) {
+        super(x,y,w,h,c);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.drawOval(x,y,width,height);
+    }
+    public FigShape getFigShape() { // 形を返す
+        return FigShape.CIRCLE;
+    }
+}
---
+++ NetworkDrawEditorProto/src/Public/FillCircleFigure.java
@@ -0,0 +1,18 @@
+package Public;
+
+import java.awt.*;
+
+public class FillCircleFigure extends Figure{
+    
+    public FillCircleFigure(int x, int y, int w, int h, Color c) {
+        super(x,y,w,h,c);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x,y,width,height);
+    }
+    public FigShape getFigShape() { // 形を返す
+        return FigShape.FILLCIRCLE;
+    }
+}
