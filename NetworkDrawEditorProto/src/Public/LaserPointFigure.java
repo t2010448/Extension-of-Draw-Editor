e070f1e2cf1bb26afbcbdadf355c48ec4bf88ea7
ytx1313 <95043210+ytx1313@users.noreply.github.com>
2022-01-07 15:24:22 +0900

    Add files via upload
    レーザーポインタのFigure追加

---
+++ NetworkDrawEditorProto/src/Public/LaserPointFigure.java
@@ -0,0 +1,30 @@
+package Public;
+
+import java.awt.*;
+
+public class LaserPointFigure extends Figure {
+
+    public LaserPointFigure(int x,int y,int w,int h,Color c) {
+        super(x,y,w,h,c);
+    }
+    public LaserPointFigure() {
+        super(-10,-10,10,10,Color.RED);
+    }
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x,y,width,height);
+    }
+    public FigShape getFigShape() { // 形を返す
+        return FigShape.LASERPOINT;
+    }
+    public boolean judgein(int x, int y) {
+        double rx = width/2, ry = height/2;
+        double cx = this.x+rx, cy = this.y+ry;
+        double px = x - cx, py = rx/ry * (y - cy);
+        if( px*px + py*py <= rx*rx ) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
