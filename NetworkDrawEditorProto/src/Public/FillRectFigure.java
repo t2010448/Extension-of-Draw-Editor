83aaa2fb752c9d190cffb1cdc3207337f69f9360
ytx1313 <95043210+ytx1313@users.noreply.github.com>
2022-01-07 17:32:18 +0900

    Update FillRectFigure.java

--- NetworkDrawEditorProto/src/Public/FillRectFigure.java
+++ NetworkDrawEditorProto/src/Public/FillRectFigure.java
@@ -12,9 +12,6 @@ public class FillRectFigure extends Figure{
         g.setColor(color);
         g.fillRect(x,y,width,height);
     }
-    public FigShape getFigShape() { // 形を返す
-        return FigShape.FILLRECT;
-    }
     public boolean judgein(int x, int y) {
         if( (this.x <= x && x <= this.x+this.width) && (this.y <= y && y <= this.y+this.height) ) {
             return true;
