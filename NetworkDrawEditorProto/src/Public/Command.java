136277fe4b437f5c9fe43a0fbb4e123b2730effe
ytx1313 <95043210+ytx1313@users.noreply.github.com>
2022-02-20 17:46:03 +0900

    Update Command.java
    コメントの追加

--- NetworkDrawEditorProto/src/Public/Command.java
+++ NetworkDrawEditorProto/src/Public/Command.java
@@ -2,8 +2,8 @@ package Public;
 
 // クライアント・サーバー間でメソッドを呼び出すためのEnum
 public enum Command {
-    ADD_FIGURE,
-    SET_FIGURES,
-    DELETE_FIGURE,
-    REPLACE_FIGURE
+    ADD_FIGURE,     // 図形の追加
+    SET_FIGURES,    // 図形リストのセット
+    DELETE_FIGURE,  // 図形の削除
+    REPLACE_FIGURE  // 図形の置き換え(変形，移動)
 }
