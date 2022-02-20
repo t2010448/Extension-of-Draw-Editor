8f6d6f5e8985e0ec4cb6cbb1802205bc740da4b8
ryo <74893406+Ryo-Takamiya@users.noreply.github.com>
2022-02-20 20:16:48 +0900

    Update FreeHandFigure.java

--- NetworkDrawEditorProto/src/Public/FreeHandFigure.java
+++ NetworkDrawEditorProto/src/Public/FreeHandFigure.java
@@ -4,21 +4,29 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class FreeHandFigure extends Figure {
-    public ArrayList<Figure> dots;
+    protected ArrayList<LineFigure> line;
+    protected int x_end, y_end;
     
     public FreeHandFigure(int x,int y,Color c) {
         super(x,y,5,5,c);
-        dots = new ArrayList<Figure>();
+        line = new ArrayList<LineFigure>();
+        line.add(new LineFigure(x, y, width, height, color));
     }
 
     public void draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(3));
         g.setColor(color);
-        for(Figure f: dots) {
-            g.fillOval(f.x, f.y, width, height);
+        for(LineFigure f: line) {
+            g.drawLine(f.x, f.y, f.x_end, f.y_end);
         }
     }
     public void writeFreeHand(int x1, int y1) { //Arraylistに点を追加していく
-        Figure f = new FillCircleFigure(x1, y1, width, height, color);
-        dots.add(f);
+        LineFigure f = line.get(line.size()-1);
+        f.x_end = x1;
+        f.y_end = y1;
+        line.set(line.size()-1, f);
+        f = new LineFigure(x1, y1, width, height, color);
+        line.add(f);
     }
 }
